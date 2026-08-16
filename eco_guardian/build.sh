cee7692fda0213200a28429870fb8d8040cbf065
Prashant Mishra <prashantmishra2505@gmail.com>
2026-08-16 07:02:49 +0530

    feat: Auto-create admin superuser on build

--- eco_guardian/build.sh
+++ eco_guardian/build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Auto-create a default superuser for the Free Tier since Shell access is disabled
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
