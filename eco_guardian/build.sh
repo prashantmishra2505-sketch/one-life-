f68f59b672d87e2a34130cefd7d5efa4a6d3393c
Prashant Mishra <prashantmishra2505@gmail.com>
2026-08-16 07:06:05 +0530

    fix: Update createsuperuser script to match custom user model

--- eco_guardian/build.sh
+++ eco_guardian/build.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Auto-create a default superuser for the Free Tier since Shell access is disabled
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@example.com').exists() or User.objects.create_superuser('admin@example.com', 'Admin', 'admin123')"
